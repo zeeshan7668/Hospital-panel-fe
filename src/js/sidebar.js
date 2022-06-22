f8d42456f5900e8d3f756afb772fca589be001b7
Salman Atqiue <salmanxheikh96@gmail.com>
2022-06-22 17:42:57 +0500

    side bar

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,3 +1,14 @@
-const showSideBar = (id) => {
-    document.getElementById(id).classList.remove("hidden"); 
-};
+$(document).ready(function () {
+  $(".sidebar-menu > li.have-children a").on("click", function (i) {
+    i.preventDefault();
+    if (!$(this).parent().hasClass("active")) {
+      $(".sidebar-menu li ul").slideUp();
+      $(this).next().slideToggle();
+      $(".sidebar-menu li").removeClass("active");
+      $(this).parent().addClass("active");
+    } else {
+      $(this).next().slideToggle();
+      $(".sidebar-menu li").removeClass("active");
+    }
+  });
+});
